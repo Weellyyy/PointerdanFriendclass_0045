11594928bf33d7d30ae5f1daf12fca18d4723e04
Weellyyy <wildanprtm2304@gmail.com>
2024-05-07 17:44:12 +0700

    menambahkan pointer object mhs

---
+++ Contoh 2/Contoh 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void showNim()
+	{
+		cout << "No Induk = " << nim << endl;
+	}
+};
+
+int main()
+{
+	mahasiswa* mhs = mahasiswa{ 1 };
+}
